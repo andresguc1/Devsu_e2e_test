75b20285d72e4b51afd0536b2c8db556819db362
Andresguc <andresguc@gmail.com>
2024-04-02 08:35:08 -0500

    refac: store data test in a fixture file

--- cypress/e2e/tests/place-order.cy.js
+++ cypress/e2e/tests/place-order.cy.js
@@ -2,51 +2,48 @@ import { CartHelper } from "../pages/cart/cart.helper";
 import { CommonPageHelper } from "../pages/commons/common-page.helper";
 import { LoginHelper } from "../pages/login/login.helper";
 
-describe("Place order", () => {
-  it("Place 2 products order with a created a user", () => {
-    const username = "devsu1";
-    const password = "devsu1";
-    const cellPhoneName = "Samsung galaxy s7";
-    const laptopName = "MacBook air";
-    const user_name = "Devsu Test User";
-    const country = "Colombia";
-    const city = "Medellin";
-    const creditCard = " 4242424242424242";
-    const month = "April";
-    const year = "2024";
+before(function () {
+  cy.visit("https://www.demoblaze.com/index.html");
+  cy.fixture("testdata").then((testdata) => {
+    this.testdata = testdata;
+  });
+});
 
-    // Login
-    CommonPageHelper.navigateToTheApplication();
+it("Place 2 products order with a created a user", function () {
+  // Ensure the testdata fixture is loaded
+  expect(this.testdata).to.exist;
+
+    // Login User
     CommonPageHelper.clickOnLoginOption();
-    LoginHelper.insertUsername(username);
-    LoginHelper.insertPassword(password);
+    LoginHelper.insertUsername(this.testdata.username);
+    LoginHelper.insertPassword(this.testdata.password);
     LoginHelper.clickOnLoginButton();
-    CommonPageHelper.verifyLoggedUser(username);
+    CommonPageHelper.verifyLoggedUser(this.testdata.username);
 
-    // add Cellphone to cart
-    CommonPageHelper.getProduct(cellPhoneName);
-    CommonPageHelper.getProductNameTittle(cellPhoneName);
-    CommonPageHelper.AddToCardButtonIsVisible()
+    // Add Cellphone to cart
+    CommonPageHelper.getProduct(this.testdata.cellPhoneName);
+    CommonPageHelper.getProductNameTittle(this.testdata.cellPhoneName);
+    CommonPageHelper.AddToCardButtonIsVisible();
     CommonPageHelper.clickAddToCardButton();
 
-    // // add Laptop to cart
+    // // Add Laptop to cart
     CommonPageHelper.clickOnHomeOption();
     CommonPageHelper.clickOnLaptopsOption();
-    CommonPageHelper.getProduct(laptopName);
-    CommonPageHelper.getProductNameTittle(laptopName);
-    CommonPageHelper.AddToCardButtonIsVisible()
+    CommonPageHelper.getProduct(this.testdata.laptopName);
+    CommonPageHelper.getProductNameTittle(this.testdata.laptopName);
+    CommonPageHelper.AddToCardButtonIsVisible();
     CommonPageHelper.clickAddToCardButton();
 
     // // Place order
     CommonPageHelper.clickOnCartOption();
     CartHelper.clickPlaceOrderButton();
-    CartHelper.insertName(user_name);
-    CartHelper.insertCountry(country);
-    CartHelper.insertCity(city);
-    CartHelper.insertCreditCard(creditCard);
-    CartHelper.insertMonth(month);
-    CartHelper.insertYear(year);
-    CartHelper.clickPurchaseButton()
-    CartHelper.confirmationOrder()
+    CartHelper.insertName(this.testdata.user_name);
+    CartHelper.insertCountry(this.testdata.country);
+    CartHelper.insertCity(this.testdata.city);
+    CartHelper.insertCreditCard(this.testdata.creditCard);
+    CartHelper.insertMonth(this.testdata.month);
+    CartHelper.insertYear(this.testdata.year);
+    CartHelper.clickPurchaseButton();
+    CartHelper.confirmationOrder();
   });
-});
+// });
